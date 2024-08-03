250faf4ee9f6911821cb3ef9bf7c93be17ed60f2
Maddy <madhavaraj1111@gmail.com>
2024-08-04 00:43:50 +0530

    docs: greet part & fix: multiple page vite config

---
+++ assets/greet.js
@@ -0,0 +1,5 @@
+import {Fireworks} from "fireworks-js";
+
+const fireworksContainer=document.querySelector("#fireworks");
+const fireworks=new Fireworks(fireworksContainer,{})
+fireworks.start();
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
 // vite.config.js
-export default {
-    base: '/festive-greeting-app/'
-  }
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base:'/festive-greeting-app/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        greet: resolve(__dirname, 'greet.html'),
+      },
+    },
+  },
+})
